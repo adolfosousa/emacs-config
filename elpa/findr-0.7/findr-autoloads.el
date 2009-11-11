b3fd9be797d669f6bb526fe8d1b0d9389e49abca
Adolfo Sousa <adolfosousa@nicanor.local>
2009-11-10 23:34:12 -0500

    my emacs configurations

---
+++ elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,16 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19129 928
+;;;;;;  876252))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; findr-autoloads.el ends here
