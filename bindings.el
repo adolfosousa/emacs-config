bf7e9116e1b1bcb60b59347a69ce17ab2c3e815d
Adolfo Sousa <adolfosousa@gmail.com>
2009-12-24 17:04:40 -0200

    tired of C-x C-s to save files. Now, M-s is doing the job

--- bindings.el
+++ bindings.el
@@ -3,9 +3,9 @@
 (global-set-key (kbd "M-z") 'undo)
 (global-set-key (kbd "M-g") 'goto-line)
 (global-set-key (kbd "M-a") 'mark-whole-buffer)
+(global-set-key (kbd "M-s") 'save-buffer)
 
 ;;(global-set-key [M-right] 'end-of-line)
 ;;(global-set-key [M-left] 'beginning-of-line)
-;; (global-set-key (kbd "M-s") 'save-buffer)
 ;; (global-set-key (kbd "C-M-h") 'backward-kill-word)
 ;; (global-set-key (kbd "M-d") 'delete-region)
