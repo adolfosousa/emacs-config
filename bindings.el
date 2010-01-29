decb8c4de6422f36af6b20b6ff99caedddf6cb48
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 20:05:19 -0200

    M-s is not working properly

--- bindings.el
+++ bindings.el
@@ -1,9 +1,10 @@
 (global-set-key (kbd "C-c r") 'revert-buffer)
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
-(global-set-key (kbd "M-z") 'undo)
-(global-set-key (kbd "M-g") 'goto-line)
-(global-set-key (kbd "M-a") 'mark-whole-buffer)
-(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z")   'undo)
+(global-set-key (kbd "M-g")   'goto-line)
+(global-set-key (kbd "M-a")   'mark-whole-buffer)
 
+
+;; (global-set-key (kbd "M-s") 'save-buffer)
 ;; (global-set-key [M-right] 'end-of-line)
 ;; (global-set-key [M-left] 'beginning-of-line)
