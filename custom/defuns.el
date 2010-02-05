90c2606433f1c879b8af5c774176411685c52188
Adolfo Sousa <adolfosousa@gmail.com>
2010-02-05 17:18:59 -0200

    customizing vendor function

--- custom/defuns.el
+++ custom/defuns.el
@@ -104,15 +104,14 @@ Symbols matching the text at point are put first in the completion list."
   (revert-buffer t t t)
   )
 
-;for loading libraries in from the vendor directory
 (defun vendor (library)
   (let* ((file (symbol-name library))
          (normal (concat "~/.emacs.d/vendor/" file))
          (suffix (concat normal ".el"))
-         (defunkt (concat "~/.emacs.d/defunkt/" file)))
+         (custom (concat "~/.emacs.d/custom/" file)))
     (cond
      ((file-directory-p normal) (add-to-list 'load-path normal) (require library))
      ((file-directory-p suffix) (add-to-list 'load-path suffix) (require library))
      ((file-exists-p suffix) (require library)))
-    (when (file-exists-p (concat defunkt ".el"))
-      (load defunkt))))
+    (when (file-exists-p (concat custom ".el"))
+      (load custom))))
