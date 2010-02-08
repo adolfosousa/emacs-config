473c89d2b0d77a9cd4e24df0130008ee52c9a37a
Adolfo Sousa <adolfosousa@gmail.com>
2010-02-08 19:35:03 -0200

    added yasnippets-rspec

--- custom/yasnippet.el
+++ custom/yasnippet.el
@@ -1,3 +1,3 @@
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
-;(yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
