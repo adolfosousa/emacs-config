669052609051e9c530114b1538d591d76c915f1d
Adolfo Sousa <adolfosousa@gmail.com>
2010-02-08 00:56:02 -0200

    fixed installation file

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
-#`git submodule update --init`
-#`cd vendor/rinari && git submodule update --init`
-#`cd ../..`
-#`cd vendor/cedet && make MAKEINFO=echo && make install`
-#`cd ../..`
+`git submodule update --init`
+`cd vendor/rinari && git submodule update --init`
+`cd ../..`
+`cd vendor/cedet && make MAKEINFO=echo && make install`
+`cd ../..`
