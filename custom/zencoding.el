a67044406a41b108a633493739af9c6798957786
Adolfo Sousa <adolfosousa@gmail.com>
2010-02-19 03:46:42 -0200

    fixing installation and configuration of zencoding-mode

--- custom/zencoding.el
+++ custom/zencoding.el
@@ -1,3 +1,3 @@
-(add-to-list 'load-path "~/emacs.d/vendor/zencoding-mode/")
+(add-to-list 'load-path "~/emacs.d/vendor/zencoding/")
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
